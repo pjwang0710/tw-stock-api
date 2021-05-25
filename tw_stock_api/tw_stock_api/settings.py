52a75e50281415807029e63ad52700c6bff8c8e2
pjwang <pjwang0710@gmail.com>
2021-05-25 12:46:28 +0800

    static

--- tw_stock_api/tw_stock_api/settings.py
+++ tw_stock_api/tw_stock_api/settings.py
@@ -24,11 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z%srw&!h8^hls3rqy4yrt#7ju$j)m%+smyjh_$udfca6hv6rw1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+DEBUG = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 ALLOWED_HOSTS = [
     '0.0.0.0'
@@ -136,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
