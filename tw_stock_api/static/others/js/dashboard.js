c3b526a4317b440b601beac3cc1681b210c8ce0b
pjwang <pjwang0710@gmail.com>
2021-05-25 14:04:47 +0800

    change host

--- tw_stock_api/static/others/js/dashboard.js
+++ tw_stock_api/static/others/js/dashboard.js
@@ -1,7 +1,7 @@
 var get_tw_stock_info = (user_id) => {
     $.ajax({
         type: "GET",
-        url: "http://localhost:8000/stocks/get_api_user_count/"+user_id,
+        url: "http://appworks.octave.vip/stocks/get_api_user_count/"+user_id,
         contentType: "application/json",
         success: function (data) {
             console.log(data);
