5dfd3064cb9bd817acd26f030d2e681b9afa1467
pjwang <pjwang0710@gmail.com>
2021-06-02 09:17:42 +0800

    Remove slash from user api

--- tw_stock_api/users/urls.py
+++ tw_stock_api/users/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('get_users/', views.get_users, name='get_users'),
+    path('get_users', views.get_users, name='get_users'),
     path('get_user_detail/<str:pk>', views.get_user_detail, name='get_user_detail'),
-    path('insert_user/', views.insert_user, name='insert_user'),
-    path('login/', views.login, name='user_login'),
+    path('insert_user', views.insert_user, name='insert_user'),
+    path('login', views.login, name='user_login'),
     path('update_user/<str:pk>', views.update_user, name='update_user'),
     path('delete_user/<str:pk>', views.delete_user, name='delete_user'),
     path('update_secret_key/<str:pk>', views.update_user_secret, name='update_secret_key'),
